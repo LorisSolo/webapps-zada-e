c82ebff3fc5138b6bb91249ada55a5ed9b33e3ca
LorisSolo <loris_solo@hotmail.com>
2022-10-10 12:17:57 +0200

    Rješen Zadatak 5

---
+++ Zadatak5.js
@@ -0,0 +1,19 @@
+let array1 = [];
+let arraytest = [];
+array1 =[1,2,3,4,5,6,7,8,9];
+function provjera(array){
+    for (let i =0; i<array.length;i++){
+        
+        if (array[i]%3==0){
+            arraytest.push(array[i]);
+            
+        }
+    }
+    
+        console.log(array1 + " -> "  + " Brojevi dijeljivi s 3: " + arraytest.join(" "));
+
+      
+}
+
+
+provjera(array1)
